4cdce5b3cb883bb5af0a8af28e97154f3968a7e1
K <117524908+0x0FFH@users.noreply.github.com>
2022-11-06 10:55:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,8 +48,8 @@ typedef unsigned short US;
 typedef unsigned int   UI;
 using namespace std;
 
-template <class Template> void alloc(Template*& next_block, int block_count) {
-    next_block = (Template*)calloc(block_count, sizeof(Template));
+template <class Template> void alloc(Template*& next_block, int block) {
+    next_block = (Template*)calloc(block, sizeof(Template));
     if (!next_block) {
         static_cast<void>(fprintf(stderr, "out of memory\n")), exit(1);
     }
@@ -68,7 +68,7 @@ protected:
     static int general_table[256];
     
 public:
-    State_Machine(int block_count = 256);
+    State_Machine(int block = 256);
     
     int next_block(int state) {
         assert(state >= 0 && state < state_number);
@@ -76,22 +76,22 @@ public:
     }
 
     void update(int rm_vector, int limit = 255) {
-        int block_count = state_count[previous_state] & 255, next_block = state_count[previous_state] >> 14;
-        if (block_count < limit) {
+        int block = state_count[previous_state] & 255, next_block = state_count[previous_state] >> 14;
+        if (block < limit) {
             ++state_count[previous_state];
-            state_count[previous_state] += ((rm_vector << 18) - next_block) * general_table[block_count] & 0xffffff00;
+            state_count[previous_state] += ((rm_vector << 18) - next_block) * general_table[block] & 0xffffff00;
         }
     }
 };
 
 int State_Machine::general_table[256] = { 0 };
 
-State_Machine::State_Machine(int block_count) : state_number(block_count), previous_state(0) {
+State_Machine::State_Machine(int block) : state_number(block), previous_state(0) {
     alloc(state_count, state_number);
     
     for (int i = 0; i < state_number; ++i) {
-        UI block_count = (i & 1) * 2 + (i & 2) + (i >> 2 & 1) + (i >> 3 & 1) + (i >> 4 & 1) + (i >> 5 & 1) + (i >> 6 & 1) + (i >> 7 & 1) + 3;
-        state_count[i] = block_count << 28 | 6;
+        UI block = (i & 1) * 2 + (i & 2) + (i >> 2 & 1) + (i >> 3 & 1) + (i >> 4 & 1) + (i >> 5 & 1) + (i >> 6 & 1) + (i >> 7 & 1) + 3;
+        state_count[i] = block << 28 | 6;
     }
     
     if (general_table[0] == 0) {
